30d477c1df68165c778b1f86ce67bb200d376c4c
Meshack Korir <meshackkorir100@gmail.com>
2023-06-07 23:17:17 +0300

    seeding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
+  before do
+    response.headers["Access-Control-Allow-Origin"]="*"
+  end
+  get '/project' do
+end
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/locations' do 
+    locations = Location.all
+        locations.to_json
   end
 
 end
