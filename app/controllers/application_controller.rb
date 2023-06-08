660656310fcefcb5b8174ff53167e17a7c9709b1
Meshack Korir <meshackkorir100@gmail.com>
2023-06-08 21:13:16 +0300

    deleted crud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,31 +3,36 @@ class ApplicationController < Sinatra::Base
   # Add routes here
   # books
   get "/books" do 
-    Book.all.to_json
-  end  
-
+    books = Book.all
+    books.to_json
+  end
+  
   post "/books" do
     book = Book.create(
       name: params[:name],
       author: params[:author],
       quantity: params[:quantity],
-      price: params[:price],
-      created_at: params[:created_at],
-      updated_at: params[:updated_at]
+      price: params[:price]
     )
-    user.to_json
+    book.to_json
   end
 
   patch "/books/:id" do
     book = Book.find(params[:id])
-    user.update(
+    book.update(
       name: params[:name],
       author: params[:author],
       quantity: params[:quantity],
-      price: params[:price],
-      created_at: params[:created_at],
-      updated_at: params[:updated_at]
+      price: params[:price]
     )
     book.to_json
   end
-end
+
+  delete "/books/:id" do
+    book = Book.find(params[:id])
+    book.destroy
+    "Book deleted successfully"
+  end
+  end
+  
+
