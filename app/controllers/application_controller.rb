5e36db8c548d33ba3a8c8df98acd6c82a8aa2633
Meshack Korir <meshackkorir100@gmail.com>
2023-06-08 06:27:58 +0300

    connected db to ruby

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  before do
-    response.headers["Access-Control-Allow-Origin"]="*"
-  end
-  get '/project' do
-end
-  # Add your routes here
-  get '/locations' do 
-    locations = Location.all
-        locations.to_json
-  end
+#   before do
+#     response.headers["Access-Control-Allow-Origin"]="*"
+#   end
+#   get '/project' do
+# end
+#   # Add your routes here
+#   get '/locations' do 
+#     locations = Location.all
+#         locations.to_json
+#   end
 
 end
