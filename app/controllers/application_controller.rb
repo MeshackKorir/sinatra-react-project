ca1ba6835ebdd25ad424fcf10e7071d6aabbb566
Meshack Korir <meshackkorir100@gmail.com>
2023-06-08 20:34:08 +0300

    updated crud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,33 @@
 class ApplicationController < Sinatra::Base
   
   # Add routes here
+  # books
   get "/books" do 
     Book.all.to_json
   end  
+
+  post "/books" do
+    book = Book.create(
+      name: params[:name],
+      author: params[:author],
+      quantity: params[:quantity],
+      price: params[:price],
+      created_at: params[:created_at],
+      updated_at: params[:updated_at]
+    )
+    user.to_json
+  end
+
+  patch "/books/:id" do
+    book = Book.find(params[:id])
+    user.update(
+      name: params[:name],
+      author: params[:author],
+      quantity: params[:quantity],
+      price: params[:price],
+      created_at: params[:created_at],
+      updated_at: params[:updated_at]
+    )
+    book.to_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,18 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 puts "creating books..."
-Book.create(name: "Meshack", author: "Ezra", quantity: "500", price: "1100" )
-Book.create(name: "Mathew", author: "Rebbecca", quantity: "600", price: "1200" )
-Book.create(name: "Andrew", author: "Salima", quantity: "700", price: "1300" )
-Book.create(name: "Sammy", author: "Tate", quantity: "800", price: "1400" )
-Book.create(name: "Dennis", author: "Felix", quantity: "900", price: "1500" )
-Book.create(name: "Brian", author: "Stephen", quantity: "100", price: "1000" )
+Book.create(name: "Blossoms of Savannah", author: "Ezra", quantity: "500", price: "1100" )
+Book.create(name: "Be Back Shortly", author: "Rebbecca", quantity: "600", price: "1200" )
+Book.create(name: "One in a million", author: "Salima", quantity: "700", price: "1300" )
+Book.create(name: "Kidagaa", author: "Tate", quantity: "800", price: "1400" )
+Book.create(name: "Damu nyeusi", author: "Felix", quantity: "900", price: "1500" )
+Book.create(name: "Walking Dead", author: "Stephen", quantity: "100", price: "1000" )
 
+puts "creating users...."
+User.create(name: "Meshack", user_id: "1000", age: "21")
+User.create(name: "Mathew", user_id: "900", age: "20")
+User.create(name: "Brian", user_id: "700", age: "22")
+User.create(name: "Cynthia", user_id: "290", age: "19")
+User.create(name: "Collins", user_id: "555", age: "15")
+User.create(name: "Dom", user_id: "899", age: "31")
 puts "✅ Done seeding!"
