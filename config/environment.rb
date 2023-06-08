8dc9eaf13de3a052fec674c955a99f9f46a6c671
Meshack Korir <meshackkorir100@gmail.com>
2023-06-08 06:25:03 +0300

    required relatives

--- config/environment.rb
+++ config/environment.rb
@@ -7,4 +7,6 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
 
 # Require in all files in 'app' directory
-require_all 'app'
+require_relative "store.rb"
+require_relative "location.rb"
+require_relative "user.rb"
